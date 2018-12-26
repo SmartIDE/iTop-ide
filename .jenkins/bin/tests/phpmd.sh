ebdfc34d53a07c2b7b00828db4e7a4f26e82bd28
bruno DA SILVA <bruno.dasilva@combodo.com>
2018-12-26 15:15:32 +0100

    bugfix: the correct path is now used

--- .jenkins/bin/tests/phpmd.sh
+++ .jenkins/bin/tests/phpmd.sh
@@ -1,9 +1,9 @@
-vendor/bin/phpmd ../core        html  unusedcode  --reportfile ../var/test/phpmd-core.xml  --suffixes php
+test/vendor/bin/phpmd ./core        html  unusedcode  --reportfile ./var/test/phpmd-core.xml  --suffixes php
 
-vendor/bin/phpmd ../application html  unusedcode  --reportfile ../var/test/phpmd-application.xml  --suffixes php
+test/vendor/bin/phpmd ./application html  unusedcode  --reportfile ./var/test/phpmd-application.xml  --suffixes php
 
-vendor/bin/phpmd ../datamodels  html  unusedcode  --reportfile ../var/test/phpmd-datamodels.xml  --suffixes php
+test/vendor/bin/phpmd ./datamodels  html  unusedcode  --reportfile ./var/test/phpmd-datamodels.xml  --suffixes php
 
-vendor/bin/phpmd ../synchro     html  unusedcode  --reportfile ../var/test/phpmd-synchro.xml  --suffixes php
+test/vendor/bin/phpmd ./synchro     html  unusedcode  --reportfile ./var/test/phpmd-synchro.xml  --suffixes php
 
-vendor/bin/phpmd ../webservices html  unusedcode  --reportfile ../var/test/phpmd-webservices.xml  --suffixes php
+test/vendor/bin/phpmd ./webservices html  unusedcode  --reportfile ./var/test/phpmd-webservices.xml  --suffixes php
