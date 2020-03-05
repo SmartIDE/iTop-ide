992eacd4753919c6fbe1f32f458e748cefa5493e
odain <olivier.dain@combodo.com>
2020-03-05 15:00:02 +0100

    bash enhancements

--- .jenkins/bin/tests/security_check.sh
+++ .jenkins/bin/tests/security_check.sh
@@ -3,7 +3,7 @@ DIR=$(dirname $0)
 mkdir "$DIR/securityChecker" || true
 mkdir "$DIR/var/test" -p || true
 
-cd securityChecker
+cd $DIR/securityChecker
 
 composer.phar require --dev sensiolabs/security-checker --no-interaction
 php vendor/bin/security-checker security:check $DIR/composer.lock --no-interaction  | tee $DIR/var/test/security_check.log
