08e89fe93b00991c348a7650d5c4ce94b0b5f5e0
bruno DA SILVA <bruno.dasilva@combodo.com>
2020-03-05 14:16:13 +0100

    CI bugfix

--- .jenkins/bin/tests/security_check.sh
+++ .jenkins/bin/tests/security_check.sh
@@ -4,5 +4,5 @@ mkdir "var/test" -p || true
 cd securityChecker
 
 composer.phar require --dev sensiolabs/security-checker --no-interaction
-php vendor/bin/security-checker security:check ../composer.lock --no-interaction  | tee ../var/test/security_check.log
+php vendor/bin/security-checker security:check ../composer.lock --no-interaction  | tee var/test/security_check.log
 
